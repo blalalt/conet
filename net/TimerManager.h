d42b10575e3a37e4de2c468d4b6d7d397c2eaa3f
blalalt <blalalt@163.com>
2020-05-26 20:40:51 +0800

    TimerManager

--- net/TimerManager.h
+++ net/TimerManager.h
@@ -1,14 +1,35 @@
 /*
  * @Description: 
+ * 1.采用timerfd_*将超时任务转换成文件描述符进行监听
+   2.当时间一到，timerfd变为可读，相应的Channel调用回调函数（TimerQueue::handleRead）
+   3.回调函数中将所有在TimerQueue中的超时任务找出，一次调用其回调函数
+   4.对于周期性定时任务，再添加回TimerQueue中
  * @version: 
  * @Author: blalalt
  * @Date: 2020-05-22 08:46:16
  */ 
-
+#include <set>
+#include <Timer.h>
+#include <memory>
+#include <vector>
+#include "../utils/TimeStamp.h"
 namespace conet {
 
 class TimerManager {
 //TODO: 完成定时器管理
+    TimerManager();
+    ~TimerManager() = default;
+
+    TimerId add_timer(TimeStamp when, TimerCallback cb, double interval);
+    void cancle(TimerID tid);
+private:
+    // 处理定时器句柄的可读事件，即有定时器激活到期，内部调用tick处理到期定时器
+    void handle_read(); 
+    bool insert(); // 定时器列表中插入定时器
+    void tick(); // 执行到期的定时器
+private:
+    struct Entry;
+    typedef std::vector<Entry> TiemrList;
 
 };
 }
